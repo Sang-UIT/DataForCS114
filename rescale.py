6cba486c34bf6bb2c0243d0d40056bc781b4bc2c
Sang-UIT <20520736@gm.uit.edu.vn>
2023-07-01 11:55:47 +0700

    change folder 36

--- rescale.py
+++ rescale.py
@@ -1,7 +1,8 @@
 import cv2
-for i in range(1,45):
+for i in range(36,37):
     for j in range(1,101):
-        path=i+'\\' + j + '.jpg'
+        path=str(i)+'\\' +str(i)+"_" + str(j) + '.jpg'
+        print(path)
         img=cv2.imread(path)
         img_66=cv2.resize(img, (600,600))
         cv2.imwrite(path,img_66)
